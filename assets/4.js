f2541e6957d9cb63e935c20e9d4bd7c6e85e041a
Cat <141873540+DTACat@users.noreply.github.com>
2024-08-30 20:42:43 +1200

    changes

--- assets/4.js
+++ assets/4.js
@@ -1,10 +1,39 @@
-setTimeout(() => { document.getElementById("modal-housing").classList.remove('hidden'); }, 100);
-
-function closeLostModal() {
-    document.getElementById("modal-housing").classList.add('hidden');
-    localStorage.sa_found = "true"
+function openDevModal() {
+    document.getElementById("dev-modal-housing").classList.remove('hidden');
 }
 
-if (localStorage.sa_found == "true") {
-    document.getElementById("modal-housing").classList.add('modal-lost-found');
+function closeLostModal() {
+    document.getElementById("dev-modal-housing").classList.add('hidden');
+}
+
+
+
+if (localStorage.not_found_found == "true") {
+    document.getElementById("404-mains-button").classList.remove('hidden'); 
+}
+
+
+
+function secret404ButtonHide() {
+    document.getElementById("404-mains-button").classList.add('hidden');
+    localStorage.not_found_found = "false"
+    console.log('hide 404 button')
+}
+
+function secret404ButtonShow() {
+    document.getElementById("404-mains-button").classList.remove('hidden');
+    localStorage.not_found_found = "true"
+    console.log('show 404 button')
+}
+
+
+
+function unreleasedProfilesPlusItemsFalse() {
+    localStorage.unreleased_profiles_plus = "false"
+    console.log('hide Unreleased Profiles Plus Items')
+}
+
+function unreleasedProfilesPlusItemsTrue() {
+    localStorage.unreleased_profiles_plus = "true"
+    console.log('show Unreleased Profiles Plus Items')
 }
