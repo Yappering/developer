0c67994eb67deefcb8b564d227f9c7d131cabc0a
Cat <141873540+DTACat@users.noreply.github.com>
2024-11-02 18:11:11 +1300

    dev 105

--- assets/4.js
+++ assets/4.js
@@ -30,7 +30,7 @@ function openLostModal() {
                 <hr style="opacity: 0">
                 <button class="refresh-button" onclick="closeLostModal()">Close</button>
                 <hr style="opacity: 0">
-                App Version: Dev 104
+                App Version: Dev 105
             </div>
         </div>
     </div>
