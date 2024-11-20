159c1a770e9b232a0f7160c4fb4b5e1b6b974b09
Cat <141873540+DTACat@users.noreply.github.com>
2024-11-21 10:39:37 +1300

    dev 136

--- assets/4.js
+++ assets/4.js
@@ -31,7 +31,7 @@ if (localStorage.full_client_rework != "true") {
                     <hr style="opacity: 0">
                     <button class="refresh-button" onclick="closeLostModal()">Close</button>
                     <hr style="opacity: 0">
-                    App Version: Dev 135
+                    App Version: Dev 136
                 </div>
             </div>
         </div>
